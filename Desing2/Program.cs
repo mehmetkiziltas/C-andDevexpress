3eaa08fbe5a7d0d1dbd2cb960c46a475768e96b4
mehmetkiziltas <mehmet13kiziltas@gmail.com>
2021-02-03 02:44:32 +0300

    Update Program.cs

--- Desing2/Program.cs
+++ Desing2/Program.cs
@@ -16,7 +16,7 @@ namespace Desing2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmAnaModul());
+            Application.Run(new FrmAdmin());
         }
     }
 }
